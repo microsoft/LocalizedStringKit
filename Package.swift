138d6baa4330b43685a1adceb1aeccc5415a9011
Dale Myers <dalemy@microsoft.com>
2020-08-12 16:19:04 +0100

    Set library to dynamic

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "LocalizedStringKit",
+            type: .dynamic,
             targets: ["LocalizedStringKit"]),
     ],
     dependencies: [],
